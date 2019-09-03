3d960e0b96231c909280e834a4b7e662ccede58b
MCMrARM <mcmrarm@gmail.com>
2019-09-03 17:44:53 +0200

    Initial stream send code

--- stream.c
+++ stream.c
@@ -1,6 +1,7 @@
 #include "stream.h"
 
 #include <malloc.h>
+#include <string.h>
 #include "session.h"
 
 static ssize_t _rxpc_stream_read(nghttp2_session *session, int32_t stream_id, uint8_t *buf, size_t length,
@@ -11,6 +12,8 @@ struct rxpc_stream *rxpc_stream_open(struct rxpc_session *session, struct rxpc_s
     struct rxpc_stream *ret = malloc(sizeof(struct rxpc_stream));
     ret->session = session;
     ret->cbs = *cbs;
+    ret->send_data_begin = NULL;
+    ret->send_data_end = NULL;
     provider.source.ptr = session;
     provider.read_callback = _rxpc_stream_read;
     ret->id = nghttp2_submit_request(session->session, NULL, NULL, 0, &provider, ret);
@@ -22,8 +25,46 @@ struct rxpc_stream *rxpc_stream_open(struct rxpc_session *session, struct rxpc_s
     return ret;
 }
 
+struct rxpc_stream_pending_data *rxpc_stream_send_alloc(size_t size) {
+    struct rxpc_stream_pending_data *ret = malloc(sizeof(struct rxpc_stream_pending_data) + size);
+    ret->next = NULL;
+    ret->offset = 0;
+    ret->size = size;
+    return ret;
+}
+
+void rxpc_stream_send_free(struct rxpc_stream_pending_data *buf) {
+    free(buf);
+}
+
+void rxpc_stream_send_raw(struct rxpc_stream *stream, struct rxpc_stream_pending_data *buffer) {
+    if (!buffer->size) {
+        rxpc_stream_send_free(buffer);
+        return;
+    }
+    if (!stream->send_data_begin)
+        stream->send_data_begin = buffer;
+    if (stream->send_data_end)
+        stream->send_data_end->next = buffer;
+    stream->send_data_end = buffer;
+    nghttp2_session_resume_data(stream->session->session, stream->id);
+}
+
 static ssize_t _rxpc_stream_read(nghttp2_session *session, int32_t stream_id, uint8_t *buf, size_t length,
         uint32_t *data_flags, nghttp2_data_source *source, void *user_data) {
-    *data_flags = NGHTTP2_DATA_FLAG_EOF | NGHTTP2_DATA_FLAG_NO_END_STREAM;
-    return 0;
+    struct rxpc_stream *stream = (struct rxpc_stream *) user_data;
+    struct rxpc_stream_pending_data *data = stream->send_data_begin;
+    if (!data)
+        return -NGHTTP2_ERR_DEFERRED;
+    if (length > data->size - data->offset)
+        length = data->size - data->offset;
+    memcpy(buf, &data->data[data->offset], length);
+    data->offset += length;
+    if (data->offset == data->size) {
+        stream->send_data_begin = data->next;
+        if (stream->send_data_end == data)
+            stream->send_data_end = NULL;
+        rxpc_stream_send_free(data);
+    }
+    return length;
 }
--- stream.h
+++ stream.h
@@ -2,9 +2,11 @@
 #define RXPC_STREAM_H
 
 #include <stdint.h>
+#include <stddef.h>
 
 struct rxpc_session;
 struct rxpc_stream;
+struct rxpc_stream_pending_data;
 
 typedef void (*rxpc_stream_opened_cb)(struct rxpc_stream *);
 
@@ -15,8 +17,16 @@ struct rxpc_stream {
     struct rxpc_session *session;
     int32_t id;
     struct rxpc_stream_callbacks cbs;
+    struct rxpc_stream_pending_data *send_data_begin, *send_data_end;
+};
+struct rxpc_stream_pending_data {
+    struct rxpc_stream_pending_data *next;
+    size_t offset, size;
+    char data[];
 };
 
 struct rxpc_stream *rxpc_stream_open(struct rxpc_session *session, struct rxpc_stream_callbacks *cbs);
+struct rxpc_stream_pending_data *rxpc_stream_send_alloc(size_t size);
+void rxpc_stream_send_raw(struct rxpc_stream *stream, struct rxpc_stream_pending_data *buffer);
 
 #endif //RXPC_STREAM_H
